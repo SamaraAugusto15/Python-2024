33f93970b7252abc67dfcfc575014a1c3978e757
Samara Pereira Augusto <samaraaugusto1511@hotmail.com>
2025-02-21 12:42:36 -0400

    func02 att

--- loteria/loteria_02.py
+++ loteria/loteria_02.py
@@ -1,14 +1,18 @@
+def valida_entrada():
+    while True:
+        try:
+            numero = int(input("Entre com um número entre 1 e 15: "))
+            return numero
+        except ValueError:
+            print("coloque um numero valido")
+
+
 numero_sorte = 7
 
 for i in range(3):
     
-    while True:
-        try:
-            numero = int(input("Entre com um número entre 1 e 15: "))
-            break
-        except ValueError:
-            print("coloque um numero valido")
-
+    numero = valida_entrada()
+    
     if numero == numero_sorte:
         print("Você acertou")
         break
