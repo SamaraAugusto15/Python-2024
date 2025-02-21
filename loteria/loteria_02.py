ab93352a22a2c552bdfa62fb6e36483251740bed
Samara Pereira Augusto <samaraaugusto1511@hotmail.com>
2025-02-21 13:11:02 -0400

    loteria att

--- loteria/loteria_02.py
+++ loteria/loteria_02.py
@@ -2,22 +2,29 @@ def valida_entrada():
     while True:
         try:
             numero = int(input("Entre com um número entre 1 e 15: "))
-            return numero
+        
         except ValueError:
-            print("coloque um numero valido")
+            print("O animal, eu pedi um número!")
+            continue
+
+        if 1 <= numero <= 15:
+            return numero
+        
+        else:
+            print("Entre com um número válido.")
 
 
 numero_sorte = 7
 
 for i in range(3):
-    
+
     numero = valida_entrada()
-    
+
     if numero == numero_sorte:
-        print("Você acertou")
+        print("Você acertou!!")
         break
+
     elif numero > numero_sorte:
-        print("Você errou. Tente um numero menor")
+        print("Você errou! Tente um número menor!")
     else:
-        print("Você errou. Tente um numero maior")
-        
+        print("Você errou! Tente um número maior")
